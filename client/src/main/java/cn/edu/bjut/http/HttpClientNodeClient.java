2dce9259a26986f37f057985adcf50c93151d5ea
郑天祺 <270490096@qq.com>
2020-07-15 17:44:46 +0800

    测试post 带headers

--- client/src/main/java/cn/edu/bjut/http/HttpClientNodeClient.java
+++ client/src/main/java/cn/edu/bjut/http/HttpClientNodeClient.java
@@ -3,6 +3,9 @@ package cn.edu.bjut.http;
 import cn.edu.bjut.entity.HttpClientResult;
 import cn.edu.bjut.utils.HttpClientUtils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author ztq
  */
@@ -23,6 +26,17 @@ public class HttpClientNodeClient {
             System.out.println("请求服务节点，POST失败：" + e.getMessage());
         }
 
-
+        try {
+            Map<String, String> headers = new HashMap<String, String>();
+            headers.put("Cookie", "NLSDHFJHSALDBULSDMADSLINF");
+            headers.put("Connection", "keep-alive");
+            headers.put("Accept", "application/json");
+            headers.put("Accept-Language", "zh-CN,zh;q=0.9");
+            headers.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
+            HttpClientResult httpPostResult = HttpClientUtils.doPost("http://127.0.0.1:8080/http/httpPostRequest", headers, null);
+            System.out.println(httpPostResult.toString());
+        } catch (Exception e) {
+            System.out.println("请求服务节点，POST失败：" + e.getMessage());
+        }
     }
 }
