6bfb1c6aaacedd2e2baf1c275333ae3bf4b191ca
zhengtianqi <zhengtianqi@360.cn>
2021-01-11 11:06:44 +0800

    data analysis

---
+++ checkpoint-data-analysis/src/test/java/cn/edu/bjut/CheckpointDataAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.bjut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckpointDataAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
